9d7e8c7d15f91a11b195be88bf1bf58ea341dda7
TontonCyrille <tontoncy@gmail.com>
2021-06-25 23:34:23 +0200

    Modifications des classes de config et de démarrage du projet

--- src/main/java/com/example/kata/config/DatabaseConfig.java
+++ src/main/java/com/example/kata/config/DatabaseConfig.java
@@ -13,7 +13,6 @@ import org.springframework.context.annotation.PropertySource;
 import org.springframework.orm.jpa.JpaTransactionManager;
 import org.springframework.orm.jpa.LocalContainerEntityManagerFactoryBean;
 import org.springframework.transaction.PlatformTransactionManager;
-
 import javax.persistence.EntityManagerFactory;
 import javax.sql.DataSource;
 import java.util.HashMap;
